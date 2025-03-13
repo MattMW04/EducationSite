93d2da098162e8c0feb809e402fec439e2f4bb5b
MattMW04 <6wilcm63@solent.ac.uk>
2025-03-13 22:46:48 +0000

    Fixed Build Errors

--- app/api/myRoutes/user/[userId]/route.ts
+++ app/api/myRoutes/user/[userId]/route.ts
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/server/connectDB.mjs';
 import Quiz from '@/server/models/Quiz.mjs';
 
-export async function GET(request: Request, { params }: { params: { userId: string } }) {
+export async function GET(request: NextRequest, context: { params: Promise<{ userId: string }> }): Promise<NextResponse> {
+  const { userId } = await context.params;
+
   await connectDB();
-  const userQuizzes = await Quiz.find({ userId: params.userId });
+  const userQuizzes = await Quiz.find({ userId: userId });
   return NextResponse.json(userQuizzes, { status: 200 });
 }
