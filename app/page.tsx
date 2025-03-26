56f59f4344210029f2f71297779710fb71aa28b1
MattMW04 <6wilcm63@solent.ac.uk>
2025-03-26 17:19:23 +0000

    Added changes to Edit and add Quiz Form to improve checkbox styling, added things to base page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-
-
-
-
 const HomePage = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen bg-background text-white">
       {/* Main Content */}
-      <main className="flex-grow p-4 bg-background">
-        <h1 className="text-2xl font-bold mb-4 text-headerText">Home Page</h1>
-        <p className="mb-4 text-bodyText">This is the home page in Next.js</p>
+      <main className="flex-grow p-8 text-center mt-4">
+        <h1 className="text-3xl font-semibold mb-6 text-headerText">Welcome to Access ED-UK</h1>
+        <p className="text-lg mb-6 text-bodyText">
+          Discover a platform designed to enhance your education experience. 
+          Track your progress, access resources, and connect with others.
+        </p>
       </main>
+
+      
     </div>
   );
 };
-
 
 export default HomePage;
 
