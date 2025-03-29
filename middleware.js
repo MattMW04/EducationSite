5bb729fbd8dd852b536c1e6f4b06005b0ab51c1e
MattMW04 <6wilcm63@solent.ac.uk>
2025-03-29 01:10:36 +0000

    Middleware working, now protecting all routes and passing cookie

--- middleware.js
+++ middleware.js
@@ -62,6 +62,7 @@ export const config = {
         "/YourQuizzes/:path*", 
         "/Quizzes/:path*", 
         "/quiz/:path*", 
+        "/createQuiz/:path*",
         "/editQuiz/:path*", 
         "/Dashboard/:path*",
         "/Account/:path*",
