351a17171c33bc5961a639ae17e46f79e8fa10ca
MattMW04 <6wilcm63@solent.ac.uk>
2025-04-01 19:12:57 +0100

    added dropdown menu styling

--- app/components/Navbar/MobileMenu.tsx
+++ app/components/Navbar/MobileMenu.tsx
@@ -14,7 +14,7 @@ interface MobileMenuProps {
     navLinks: { href: string; label: string; subLinks?: { href: string; label: string }[] }[]; // subLinks can be removed if not needed
 }
 
-const MobileMenu: React.FC<MobileMenuProps> = ({handleToggle, isOpen, navLinks}) => {
+const MobileMenu: React.FC<MobileMenuProps> = ({ handleToggle, isOpen, navLinks }) => {
     const pathname = usePathname();
     return (
         <div className={`fixed top-0 right-0 h-full w-64 bg-nav text-white p-4 z-50 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out`}>
@@ -26,11 +26,30 @@ const MobileMenu: React.FC<MobileMenuProps> = ({handleToggle, isOpen, navLinks})
             </div>
             <div className="flex-1 flex flex-col">
                 {navLinks.map((link, index) =>
-                    link.label === "Logout" ? (
+                    link.subLinks ? (
+                        <div key={index} className="dropdown dropdown-bottom dropdown-center">
+                            <div
+                                tabIndex={0}
+                                role="button"
+                                className="flex-1 text-center btn btn-ghost"
+                            >
+                                {link.label}
+                            </div>
+                            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                                {link.subLinks.map((subLink, subIndex) => (
+                                    <li key={subIndex}>
+                                        <Link href={subLink.href} className={`${pathname === subLink.href ? 'border-b-2 border-white' : ''}`}>
+                                            {subLink.label}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ) : link.label === "Logout" ? (
                         <button
                             key={index}
                             onClick={() => signOut()}
-                            className="flex-1 text-center btn btn-ghost" >
+                            className="flex-1 text-center btn btn-ghost">
                             {link.label}
                         </button>
                     ) : (
